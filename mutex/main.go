52d167e8116cebddada1d65dcf1381d97d65982d
shubham88fru <shubham88fru@yahoo.com>
2025-03-23 15:44:16 -0700

    more on mutex

--- mutex/main.go
+++ mutex/main.go
@@ -27,4 +27,53 @@ func main() {
 	wg.Wait()
 
 	fmt.Println(msg)
+
+	example2()
+}
+
+var wg2 sync.WaitGroup
+
+type Income struct {
+	Source string
+	Amount int
+}
+
+func example2() {
+	//variable for bank balance.
+	var bankBalance int
+	var balance sync.Mutex
+
+	//print out starting values.
+	fmt.Print("Initial account balance: $%d.00", bankBalance)
+
+	//define weekly revenue.
+	incomes := []Income{
+		{Source: "Main job", Amount: 500},
+		{Source: "Gifts ", Amount: 10},
+		{Source: "Part time job", Amount: 50},
+		{Source: "Investments", Amount: 100},
+	}
+	wg2.Add(len(incomes))
+
+	//loop through 52 weeks and print how much is made; keep a running total.
+	for i, income := range incomes {
+		go func(i int, income Income) {
+			defer wg2.Done()
+
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				temp := bankBalance
+				temp += income.Amount
+				bankBalance = temp
+				balance.Unlock()
+
+				fmt.Printf("Week %d: %s: $%d.00\n", week, income.Source, income.Amount)
+			}
+		}(i, income)
+	}
+	wg2.Wait()
+
+	//print out final balance.
+	fmt.Printf("Final account balance: $%d.00", bankBalance)
+	fmt.Println()
 }
